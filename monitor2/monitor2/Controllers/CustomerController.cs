ad62c4770830efd976d8f42af145f60bbdaf3da8
sametyegen26@gmail.com <sametyegen26@gmail.com>
2021-07-14 17:25:29 +0300

    ilk commit

---
+++ monitor2/monitor2/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace monitor2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
